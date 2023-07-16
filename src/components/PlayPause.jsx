a70cd6cd5e254ffd1af0af099a1cacba61398200
Joy Sarkar <joysarkar8171@gmail.com>
2023-07-16 23:28:03 +0530

    play pause icon toggle added

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,9 +1,10 @@
 import {BiPlayCircle } from "react-icons/bi"
 import {AiOutlinePauseCircle} from "react-icons/ai"
+
 const PlayPause = (props) =>
 
 {
-  if(props.song.title===props.activeSong.title){
+  if(props.isPlaying && props.song.title===props.activeSong.title){
     return (
       <div>
         <AiOutlinePauseCircle size={70} className="text-gray-300" onClick={props.handlePause}></AiOutlinePauseCircle>
