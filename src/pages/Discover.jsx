5a82854430c890735978d05d2b874c3867f73326
Joy Sarkar <joysarkar8171@gmail.com>
2023-07-16 23:57:28 +0530

    update

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -21,7 +21,7 @@ const Discover = () => {
     return(
     <div className="flex flex-col">
         <div className="w-full flex sm:flex-row  flex-col justify-between items-center mt-4 mb-10">
-            <h1 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h1>
+            <h1 className="font-bold text-3xl text-white text-left">Play-Joy</h1>
             <select className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-4">
                 {genres.map((genre)=>{
                     return (
@@ -37,6 +37,10 @@ const Discover = () => {
             return <SongCard key={song.key}  song={song} index = {i} activeSong = {activeSong} isPlaying={isPlaying} data = {data}></SongCard>
            })}
 
+        </div>
+
+        <div>
+            <h2 className="p-5 fon text-white text-2xl">Access to the full song API cost 40 Fuckings dollars so fuck it</h2>
         </div>
 
     </div>
